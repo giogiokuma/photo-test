5d8c82ed5ef95c17b7d425ca8ada81823138c80a
Misako Ohata <misaohatadesign@yahoo.co.jp>
2018-02-16 15:17:58 +0900

    fixed untracked files

--- app.js
+++ app.js
@@ -32,6 +32,7 @@ app.use(helmet());
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
